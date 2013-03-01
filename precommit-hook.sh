a4c85ba6af945e07bf71c0253a73abd736acc4e3
David Shepherd <davidshepherd7@gmail.com>
2013-03-01 13:23:37 +0000

    Added "install" command to precommit hook

--- precommit-hook.sh
+++ precommit-hook.sh
@@ -3,6 +3,11 @@
 set -o errexit
 set -o nounset
 
+# # To install just do:
+# cd .git/hooks/
+# ln -s ../../precommit-hook.sh
+
+
 # Use autopep8 to clean up whitespace etc. (also store and print a diff of
 # what it changed)
 autopep8 . -d -r --aggressive --ignore=E702 --ignore=E226 > .pep8diff
