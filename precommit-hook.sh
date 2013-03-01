3cdd13f1c7e3435144694e1a4f3ab1f5caed040f
David Shepherd <davidshepherd7@gmail.com>
2013-03-01 13:21:20 +0000

    Added a precommit hook to run autopep8 and self tests

---
+++ precommit-hook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+# Use autopep8 to clean up whitespace etc. (also store and print a diff of
+# what it changed)
+autopep8 . -d -r --aggressive --ignore=E702 --ignore=E226 > .pep8diff
+cat .pep8diff
+patch <.pep8diff -p1
+
+echo -e "\n\n\n"
+
+# Run self tests
+nosetests --all-modules --processes=8
