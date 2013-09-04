047d9f748aa14cb9497b3179bab031858da9400b
David Shepherd <davidshepherd7@gmail.com>
2013-09-04 17:52:01 +0100

    Add some more example residuals: stiff ones and a constant one

--- simplellg/example_residuals.py
+++ simplellg/example_residuals.py
@@ -1,11 +1,11 @@
 # Some pairs of residuals and exact solutions for testing with:
 
-from scipy import exp, tanh
+from scipy import exp, tanh, sin, cos
 import scipy as sp
 
 def exp_residual(t, y, dydt): return y - dydt
 def exp_dydt(t, y): return y
-def exp_exact(t, y): return exp(t)
+def exp_exact(t): return exp(t)
 
 
 def exp3_residual(t, y, dydt): return 3*y - dydt
@@ -45,19 +45,43 @@ def tanh_simple_exact(t, alpha=1.0, step_time=1.0):
     return tanh(alpha*(t - step_time))
 
 # Stiff example
-def van_der_pol_residual(t, y, dydt, mu=1000):
+def van_der_pol_residual(t, y, dydt, mu=10):
     return dydt - van_der_pol_dydt(t, y, mu)
-def van_der_pol_dydt(t, y, mu=1000):
+def van_der_pol_dydt(t, y, mu=10):
     return sp.array([y[1], mu * (1 - y[0]**2)*y[1] - y[0]])
 # No exact solution for van der pol afaik
 
+# The classical example of a stiff ODE
+def stiff_damped_example_residual(t, y, dydt):
+    return dydt - stiff_damped_example_dydt(t, y)
+def stiff_damped_example_dydt(t, y):
+    return sp.dot(sp.array([[-1, 1], [1, -1000]]), y)
+
+# From G&S pg 258
+def stiff_example_residual(t, y, dydt):
+    return dydt - stiff_damped_example_dydt(t, y)
+def stiff_example_dydt(t, y):
+    return sp.dot(sp.array([[-1, 1], [1, -1000]]), y)
+def stiff_example_exact(t):
+    l1 = 1000.0010001
+    l2 = 0.998999
+    return sp.array([(-l2/(l1 - l2))*sp.exp(-l1*t) + (l1/(l1 - l2))*sp.exp(-l2*t),
+                     (l2*(l1 - 1))*sp.exp(-l1*t)/(l1 - l2) + l1*(1 - l2)*sp.exp(-l2*t)/(l1 - l2)
+                     ])
+
+
+def stiff_trig_residual(t, y, dydt):
+    return dydt - stiff_trig_dydt(t, y)
+def stiff_trig_dydt(t, y):
+    return 100 * (sin(t) - y)
+def stiff_trig_exact(t):
+    return (sin(t) - 0.01*cos(t) + 0.01*exp(-100*t))/1.0001
+
 # ODE example for paper?
 def damped_oscillation_residual(omega, beta, t, y, dydt):
-    return dydt + beta*sp.exp(-beta*t)*sp.sin(omega*t) - omega*sp.exp(-beta*t)*sp.cos(omega*t)
-
+    return dydt - damped_oscillation_dydt(omega, beta, t, y)
 def damped_oscillation_dydt(omega, beta, t, y):
-    return - damped_oscillation_residual(omega, beta, t, y, 0)
-
+    return beta*sp.exp(-beta*t)*sp.sin(omega*t) - omega*sp.exp(-beta*t)*sp.cos(omega*t)
 def damped_oscillation_exact(omega, beta, t):
     return sp.exp(-beta*t) * sp.sin(omega*t)
 
@@ -72,3 +96,11 @@ def damped_oscillation_ddydt(omega, beta, t):
     a = sp.exp(- beta*t) * sp.sin(omega * t)
     b = sp.exp(-beta * t) * sp.cos(omega * t)
     return (beta**2 - omega**2)*a - 2*omega*beta*b
+
+
+def constant_dydt(t, y):
+    return 0
+def constant_residual(t, y, dydt):
+    return dydt - constant_dydt(t, y)
+def constant_exact(t):
+    return 1
