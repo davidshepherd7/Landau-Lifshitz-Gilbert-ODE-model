29c6ec71180e2b54142de02c828981cfc3e27d2b
David Shepherd <davidshepherd7@gmail.com>
2013-09-18 17:59:55 +0100

    Lots of small changes to two_predictor.py
    * Add some imports
    
    * Add some lte functions
    
    * Correct sign of midpoint_f_approximation_error(..)
    
    * Add helper functions: is_rational, rat_as_mixed, sum_dts
    
    * Remove (un-needed) symbol creation from some functions
    
    * Add more dts to constify step
    
    * Remove some unused functions
    
    * Add some tests

--- algebra/two_predictor.py
+++ algebra/two_predictor.py
@@ -6,8 +6,13 @@ import sympy
 import scipy.misc
 import sys
 import itertools as it
+import math
 
 from sympy import Rational as sRat
+from sympy.simplify.cse_main import cse
+
+
+from pprint import pprint as pp
 from operator import mul
 from functools import partial as par
 # import sympy.simplify.simplify as simpl
@@ -28,6 +33,11 @@ def bdf2_lte(dtn, dtnm1, dddyn):
     """
     return -((dtn + dtnm1)**2/(dtn*(2*dtn + dtnm1))) * dtn**3 * dddyn/6
 
+def bdf3_lte(dtn, dtnm1, dtnm2, dddyn):
+    """Gresho and Sani pg.715, sign inverted
+    """
+    return 0 # No error to O(dtn**4) (third order)
+
 
 def ebdf2_lte(dtn, dtnm1, dddyn):
     """Gresho and Sani pg.715, sign inverted, dtn/dtnm1 corrected as in Prinja
@@ -39,16 +49,16 @@ def ebdf3_lte(dtn, dtnm1, dtnm2, dddyn):
     return 0 # no error to O(dtn**4) (third order)
 
 
+def ab2_lte(dtn, dtnm1, dddyn):
+    return (2 + 3*dtnm1/dtn) * dtn**3 * dddyn/12
+
+
 def midpoint_f_approximation_error(dtn, Fddynph):
-    return dtn**2*Fddynph/8
+    return -dtn**2*Fddynph/8
 
 
 # Helper functions
 # ============================================================
-def full_pretty(expr):
-    print sympy.pretty(sympy.collect(expr.expand(), [dddynph, Fddynph, y_np1_exact]))
-
-
 def get_a(expr):
     """Get coeff of dddynph from expression.
     """
@@ -62,16 +72,14 @@ def get_b(expr):
 
 
 def get_coeffs(expr):
-    y_np1_imr, y_np1_p2, y_np1_p3 = sympy.symbols('y_0_mp y_0_p2 y_0_p3', Real=True)
-    imr_coeff = expr.subs([(y_np1_imr, 1), (y_np1_p2,0), (y_np1_p3,0)])
-    p2_coeff = expr.subs([(y_np1_imr,0), (y_np1_p2,1), (y_np1_p3,0)])
-    p3_coeff = expr.subs([(y_np1_imr,0), (y_np1_p2,0), (y_np1_p3,1)])
-
-    return imr_coeff, p2_coeff, p3_coeff
+    imr_coeff = expr.subs([(y_np1_imr, 1), (y_np1_p2,0), (y_np1_p1,0)])
+    p2_coeff = expr.subs([(y_np1_imr,0), (y_np1_p2,1), (y_np1_p1,0)])
+    p1_coeff = expr.subs([(y_np1_imr,0), (y_np1_p2,0), (y_np1_p1,1)])
+    return imr_coeff, p2_coeff, p1_coeff
 
 
 def constify_step(expr):
-    return expr.subs([(dts[1], dts[0])])
+    return expr.subs([(dts[1], dts[0]), (dts[2], dts[0]), (dts[3], dts[0])])
 
 
 # Define symbol names
@@ -80,11 +88,61 @@ dts = sympy.symbols('Delta0:9', Real=True)
 dddynph = sympy.symbols("y'''_h", Real=True)
 Fddynph = sympy.symbols("F.y''_h", Real=True)
 y_np1_exact = sympy.symbols('y_0', Real=True)
-y_np1_imr, y_np1_p2, y_np1_p3 = sympy.symbols('y_0_mp y_0_p2 y_0_p3', Real=True)
+y_np1_imr, y_np1_p2, y_np1_p1 = sympy.symbols('y_0_imr y_0_p2 y_0_p1',
+                                              Real=True)
 
 
 # Calculate full errors
 # ============================================================
+
+def is_rational(x):
+    try:
+        # If it's a sympy object this is all we need
+        return x.is_rational
+    except AttributeError:
+        # Otherwise only integer-like objects are rational
+        return math.floor(x) == x
+
+
+def rat_as_mixed(x):
+    """Convert a rational number to (integer_part, remainder_as_fraction)
+    """
+    assert is_rational(x) # Don't want to accidentally end up with floats
+                          # in here as very long rationals!
+    x_int = int(x)
+    return x_int, sRat(x - x_int)
+
+
+def sum_dts(a, b):
+    """Get t_a - t_b in terms of dts.
+
+    e.g.
+    a = 0, b = 2:
+    t_0 - t_2 = t_{n+1} - t_{n-1} = dt_{n+1} + dt_n = dts[0] + dts[1]
+
+    a = 2, b = 0:
+    t_2 - t_0 = - dts[0] - dts[1]
+    """
+
+    # Doesn't work for negative a, b
+    assert a >= 0 and b >= 0
+
+    # if a and b are in the "wrong" order then it's just the negative of
+    # the sum with them in the "right" order.
+    if a > b:
+        return -1 * sum_dts(b, a)
+
+    # Deal with non-integer a
+    a_int, a_frac = rat_as_mixed(a)
+    if a_frac != 0:
+        return a_frac * dts[a_int] + sum_dts(a_int, b)
+
+    b_int, b_frac = rat_as_mixed(b)
+    if b_frac != 0:
+        return b_frac * dts[b_int] + sum_dts(a, b_int)
+
+    return sum(dts[a:b])
+
 
 def m16_scheme():
     """Generate dictionary of coeffs for the scheme.
@@ -230,11 +288,61 @@ if __name__ == '__main__':
 # Tests
 # ============================================================
 
-def regression_test_t17():
-    """Check that the method hasn't changed while I fiddle with stuff.
-    """
-    old = [-12/(dts[0]*(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2)), 12*(dts[0] + dts[1])/(dts[0]*dts[1]*(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2)), -12/(dts[1]*(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2)), 4/dts[0]**3, -4/dts[0]**3, 0, dts[1]*(dts[0] + dts[1])/(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2), (dts[0]**3 + 2*dts[0]**2*dts[1] + 2*dts[0]*dts[1]**2 + 2*dts[1]**3)/(2*dts[1]*(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2)), -dts[0]**3/(2*dts[1]*(dts[0]**2 + 2*dts[0]*dts[1] + 2*dts[1]**2))]
 
-    new_coeffs_flat = list(it.chain(*full_method(t17_scheme)))
-    for old, new in zip(old, new_coeffs_flat):
-        utils.assert_sym_eq(old, new.simplify())
+
+
+def test_dddy_estimates():
+
+        #
+    t = sympy.symbols('t')
+
+    equations = [
+        3*t**3,
+        sympy.tanh(t),
+        sympy.exp(-t)
+        ]
+
+
+    for exact_symb in equations:
+         yield check_dddy_estimates, exact_symb
+
+
+def test_general_two_predictor_imr_a_bit():
+    # Check the we generate imr's lte if we plug in the right times.
+    p1_steps = (0, sRat(1,2), 1)
+    p2_steps = (0, sRat(1,2), 1)
+    lte_equations, (p1_func, p2_func) = general_two_predictor(p1_steps, p2_steps)
+
+    utils.assert_sym_eq(lte_equations[0], lte_equations[2])
+    utils.assert_sym_eq(lte_equations[1], lte_equations[2])
+
+
+def test_sum_dts():
+
+    def check_sum_dts(a, b):
+
+        # Check we can swap the sign
+        utils.assert_sym_eq(sum_dts(a, b), -sum_dts(b, a))
+
+        # Starting half a step earlier
+        utils.assert_sym_eq(sum_dts(a, b+ sRat(1,2)),
+                            +sRat(1,2)*dts[b] + sum_dts(a, b))
+
+        # Check that we can split it up
+        utils.assert_sym_eq(sum_dts(a, b),
+                            sum_dts(a, b-1) + sum_dts(b-1, b))
+
+    cases = [(0, 1),
+             (5, 8),
+             (sRat(1,2), 3),
+             (sRat(2,2), 3),
+             (sRat(3,2), 3),
+             (0, sRat(4,2)),
+             ]
+
+    for a, b in cases:
+        yield check_sum_dts, a, b
+
+
+
+#
